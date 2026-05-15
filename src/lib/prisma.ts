46f508e16d4c7e27ca06b42d63f0f92fe935a953
atocodes <atocodes@gmail.com>
2026-05-15 18:08:29 +0300

    refactor: remove Prisma adapter and update PrismaClient initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "../../generated/prisma-client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
